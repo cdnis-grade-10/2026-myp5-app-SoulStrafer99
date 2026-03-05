cb79e602c7771feca0a1636d4731bab4a42f044d
Aristo Lo <dkdkdeathXXD@gmail.com>
2026-03-05 08:17:33 +0800

    login screen redirect done (link can be changed anytime)

--- StarterProjectMultipleViews/LoginViewController.swift
+++ StarterProjectMultipleViews/LoginViewController.swift
@@ -8,47 +8,51 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-            emailTextField.font = UIFont.systemFont(ofSize: 20, weight: .regular)
-            
-            let emailPlaceholderAttributes: [NSAttributedString.Key: Any] = [
-                .font: UIFont.systemFont(ofSize: 20, weight: .black),
-                .foregroundColor: UIColor.lightGray
-            ]
-            
-            emailTextField.attributedPlaceholder = NSAttributedString(
-                string: "Email",
-                attributes: emailPlaceholderAttributes
-            )
+        emailTextField.font = UIFont.systemFont(ofSize: 20, weight: .regular)
         
-            passwordTextField.font = UIFont.systemFont(ofSize: 20, weight: .regular)
-            
-            // 2. Set a custom style for the placeholder specifically
-            // This keeps the placeholder Regular weight even though the input is Bold
-            let passwordPlaceholderAttributes: [NSAttributedString.Key: Any] = [
-                .font: UIFont.systemFont(ofSize: 20, weight: .bold),
-                .foregroundColor: UIColor.lightGray
-            ]
-            
-            passwordTextField.attributedPlaceholder = NSAttributedString(
-                string: "Password",
-                attributes: passwordPlaceholderAttributes
-            )
+        let emailPlaceholderAttributes: [NSAttributedString.Key: Any] = [
+            .font: UIFont.systemFont(ofSize: 20, weight: .black),
+            .foregroundColor: UIColor.lightGray
+        ]
+        
+        emailTextField.attributedPlaceholder = NSAttributedString(
+            string: "Email",
+            attributes: emailPlaceholderAttributes
+        )
+        
+        passwordTextField.font = UIFont.systemFont(ofSize: 20, weight: .regular)
+        
+        // 2. Set a custom style for the placeholder specifically
+        // This keeps the placeholder Regular weight even though the input is Bold
+        let passwordPlaceholderAttributes: [NSAttributedString.Key: Any] = [
+            .font: UIFont.systemFont(ofSize: 20, weight: .bold),
+            .foregroundColor: UIColor.lightGray
+        ]
+        
+        passwordTextField.attributedPlaceholder = NSAttributedString(
+            string: "Password",
+            attributes: passwordPlaceholderAttributes
+        )
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         // This tells the view to stop editing, which hides the keyboard
         self.view.endEditing(true)
     }
-
-    @IBAction func loginClicked(_ sender: Any) {
-    }
-    @IBAction func signUpClicked(_ sender: Any) {
-    }
-    @IBAction func forgotPasswordClicked(_ sender: Any) {
+    @IBAction func redirectWebsite(_ sender: Any) {
+        let urlString = "https://www.cdnis.edu.hk/"
+        
+        // 2. Convert it to a real URL object
+        if let url = URL(string: urlString) {
+            // 3. Tell the iPhone to open it in the default browser
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        }
+        
+        
     }
 }
